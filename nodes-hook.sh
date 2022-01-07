bb0ad9d806beb6183330c0db74e5313efb640dbb
Jan Werner <mail@janwerner.de>
2022-01-07 18:33:01 +0100

    Auto-Commit

--- nodes-hook.sh
+++ nodes-hook.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+caddyAPI=172.25.2.170:2019
+
 if [[ $1 == "--config" ]] ; then
   cat <<EOF
 configVersion: v1
@@ -14,10 +16,10 @@ else
 
   curlResult=$(curl --write-out '%{http_code}' --silent --output /dev/null -X PATCH \
     -H "Content-Type: application/json" \
-    -d '[{"dial": "172.25.2.203:32080"}, {"dial": "172.25.2.167:32080"}]' \
-    "172.25.2.170:2019/config/apps/http/servers/srv0/routes/0/handle/0/routes/0/handle/0/upstreams/")
+    -d "${nodes}" \
+    "${caddyAPI}/config/apps/http/servers/srv0/routes/0/handle/0/routes/0/handle/0/upstreams/")
 
-  if [[ $result == "200" ]]; then
+  if [[ $curlResult == "200" ]]; then
     echo "Caddy endpoint update: OK"
   else
     echo "Caddy endpoint update: Error"
